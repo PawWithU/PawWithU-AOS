0d296f43258339127042ed665da4b34b6909621d
Seunggu Kang <alex9366@naver.com>
2024-03-29 11:18:09 +0900

    update: 프로필 수정 버튼 및 로직 추가, 관리 카드 사이즈 재설정

--- feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/screen/IntermediatorHomeScreen.kt
+++ feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/screen/IntermediatorHomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
@@ -49,6 +50,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.kusitms.connectdog.core.designsystem.component.ConnectDogFilledButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogIntermediatorTopAppBar
 import com.kusitms.connectdog.core.designsystem.component.NetworkImage
 import com.kusitms.connectdog.core.designsystem.theme.Brown5
@@ -84,7 +86,8 @@ internal data class CardItem(
 fun IntermediatorHomeScreen(
     onNotificationClick: () -> Unit,
     onSettingClick: () -> Unit,
-    onDataClick: (Int) -> Unit,
+    onManageClick: (Int) -> Unit,
+    onProfileClick: () -> Unit,
     viewModel: InterHomeViewModel = hiltViewModel()
 ) {
     LaunchedEffect(Unit) {
@@ -100,7 +103,8 @@ fun IntermediatorHomeScreen(
     ) {
         Content(
             viewModel = viewModel,
-            onClick = onDataClick
+            onManageClick = onManageClick,
+            onProfileClick = onProfileClick
         )
     }
 }
@@ -108,13 +112,19 @@ fun IntermediatorHomeScreen(
 @Composable
 private fun Content(
     viewModel: InterHomeViewModel,
-    onClick: (Int) -> Unit
+    onProfileClick: () -> Unit,
+    onManageClick: (Int) -> Unit
 ) {
     val recruitingCount = viewModel.recruitingCount.collectAsState()
     val waitingCount = viewModel.waitingCount.collectAsState()
     val progressingCount = viewModel.progressingCount.collectAsState()
     val completedCount = viewModel.completedCount.collectAsState()
-    val cnt = listOf(recruitingCount.value, waitingCount.value, progressingCount.value, completedCount.value)
+    val cnt = listOf(
+        recruitingCount.value,
+        waitingCount.value,
+        progressingCount.value,
+        completedCount.value
+    )
     val list = cnt.mapIndexedNotNull { index, value ->
         value?.let {
             CardItem(
@@ -128,20 +138,21 @@ private fun Content(
         modifier = Modifier.fillMaxWidth()
     ) {
         Spacer(modifier = Modifier.height(48.dp))
-        ProfileCard(viewModel)
-        ManageBoard(list) { onClick(it) }
+        ProfileCard(viewModel, onProfileClick)
+        ManageBoard(list) { onManageClick(it) }
     }
 }
 
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 private fun ProfileCard(
-    viewModel: InterHomeViewModel
+    viewModel: InterHomeViewModel,
+    onProfileClick: () -> Unit
 ) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(180.dp)
+            .height(172.dp)
             .background(MaterialTheme.colorScheme.primary)
     ) {
         Row(
@@ -156,12 +167,25 @@ private fun ProfileCard(
                     placeholder = painterResource(id = R.drawable.ic_default_intermediator)
                 )
                 Spacer(modifier = Modifier.height(18.dp))
-                Text(
-                    text = viewModel.intermediaryName.value,
-                    style = MaterialTheme.typography.titleSmall,
-                    color = Color.White,
-                    fontSize = 20.sp
-                )
+                Row(
+                    modifier = Modifier.wrapContentHeight(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = viewModel.intermediaryName.value,
+                        style = MaterialTheme.typography.titleSmall,
+                        color = Color.White,
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    ConnectDogFilledButton(
+                        width = 45,
+                        height = 14,
+                        text = "프로필 보기",
+                        padding = 1,
+                        onClick = onProfileClick
+                    )
+                }
                 Spacer(modifier = Modifier.height(6.dp))
                 Text(
                     text = viewModel.intro.value,
@@ -231,9 +255,9 @@ private fun ManageBoard(
                 ) { onClick(index) }
             }
         }
-        Spacer(modifier = Modifier.height(20.dp))
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             ApplyButton(onClick = {})
@@ -279,8 +303,7 @@ private fun ManageCard(
             containerColor = Color.White
         ),
         modifier = Modifier
-            .width(170.dp)
-            .height(200.dp)
+            .size(width = 150.dp, height = 180.dp)
             .clickable { onClick() }
     ) {
         Column(
@@ -318,6 +341,24 @@ private fun ManageCard(
 @Composable
 private fun test() {
     ConnectDogTheme {
-        IntermediatorHomeScreen(onNotificationClick = {}, onSettingClick = {}, {})
+        Row(
+            modifier = Modifier.wrapContentHeight(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "이동봉사 중개",
+                style = MaterialTheme.typography.titleSmall,
+                color = Color.White,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.width(6.dp))
+            ConnectDogFilledButton(
+                width = 45,
+                height = 14,
+                text = "프로필 보기",
+                padding = 1,
+                onClick = {}
+            )
+        }
     }
 }
