03bd858aed62a481fe657d9f4b467f4c6096b7ce
Seunggu Kang <alex9366@naver.com>
2024-05-06 15:40:46 +0900

    update: 백엔드 수정사항 반영

--- core/data/src/main/java/com/kusitms/connectdog/core/data/api/model/MyInfoResponseItem.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/api/model/MyInfoResponseItem.kt
@@ -1,7 +1,10 @@
 package com.kusitms.connectdog.core.data.api.model
 
 data class MyInfoResponseItem(
+    val profileImageNum: Int,
+    val nickname: String,
+    val waitingCount: Int,
     val completedCount: Int,
-    val dogStatusCount: Int,
+    val progressingCount: Int,
     val reviewCount: Int
 )
