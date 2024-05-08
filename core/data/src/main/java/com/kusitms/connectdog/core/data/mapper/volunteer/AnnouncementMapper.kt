b9ef80c3c80cdc6f467b41960a646cd726a02057
Seunggu Kang <alex9366@naver.com>
2024-05-08 22:24:57 +0900

    update: 수정된 dto 반영

--- core/data/src/main/java/com/kusitms/connectdog/core/data/mapper/volunteer/AnnouncementMapper.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/mapper/volunteer/AnnouncementMapper.kt
@@ -2,8 +2,8 @@ package com.kusitms.connectdog.core.data.mapper.volunteer
 
 import com.kusitms.connectdog.core.data.api.model.volunteer.AnnouncementHomeResponseItem
 import com.kusitms.connectdog.core.data.api.model.volunteer.AnnouncementSearchResponseItem
-import com.kusitms.connectdog.core.model.AnnouncementHome
 import com.kusitms.connectdog.core.model.Announcement
+import com.kusitms.connectdog.core.model.AnnouncementHome
 import com.kusitms.connectdog.core.util.dateRangeFormat
 
 internal fun AnnouncementHomeResponseItem.toData(): AnnouncementHome {
