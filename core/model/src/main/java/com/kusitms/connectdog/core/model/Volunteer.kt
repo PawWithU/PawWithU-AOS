3731309342767ab025bfe52a88d60e5ada128332
Seunggu Kang <alex9366@naver.com>
2024-05-12 05:09:17 +0900

    update: 수정된 dto 반영

--- core/model/src/main/java/com/kusitms/connectdog/core/model/Volunteer.kt
+++ core/model/src/main/java/com/kusitms/connectdog/core/model/Volunteer.kt
@@ -1,9 +1,9 @@
 package com.kusitms.connectdog.core.model
 
 data class Volunteer(
-    val name: String,
-    val phoneNumber: String,
-    val vehicle: String,
-    val comment: String,
+    val volunteerName: String,
+    val phone: String,
+    val createdDate: String,
+    val content: String,
     val id: Long
 )
