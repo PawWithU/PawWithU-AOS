641f1543d5b97095c494379402694ad025ef77bb
Seunggu Kang <alex9366@naver.com>
2024-05-15 16:42:43 +0900

    feat: 모집중, 완료 및 후기 탭 구현

--- feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/screen/InterProfileScreen.kt
+++ feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/screen/InterProfileScreen.kt
@@ -3,6 +3,7 @@ package com.kusitms.connectdog.feature.intermediator.screen
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +22,7 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,66 +30,72 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
+import com.kusitms.connectdog.core.designsystem.component.AnnouncementContent
+import com.kusitms.connectdog.core.designsystem.component.ConnectDogBottomButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogInformationCard
-import com.kusitms.connectdog.core.designsystem.component.ConnectDogNormalButton
-import com.kusitms.connectdog.core.designsystem.component.ConnectDogOutlinedButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
 import com.kusitms.connectdog.core.designsystem.component.DetailInfo
+import com.kusitms.connectdog.core.designsystem.component.Loading
 import com.kusitms.connectdog.core.designsystem.component.NetworkImage
+import com.kusitms.connectdog.core.designsystem.component.ReviewItemContent
 import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
 import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
 import com.kusitms.connectdog.core.designsystem.theme.Gray1
 import com.kusitms.connectdog.core.designsystem.theme.Gray2
 import com.kusitms.connectdog.core.designsystem.theme.Gray4
+import com.kusitms.connectdog.core.designsystem.theme.Gray7
+import com.kusitms.connectdog.feature.intermediator.R
+import com.kusitms.connectdog.feature.intermediator.state.InterProfileFindingUiState
+import com.kusitms.connectdog.feature.intermediator.state.InterProfileReviewUiState
+import com.kusitms.connectdog.feature.intermediator.viewmodel.InterProfileViewModel
 import kotlinx.coroutines.launch
 
-val pages = listOf("기본 정보", "후기", "근황")
+private val pages = listOf("기본 정보", "후기", "근황")
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 internal fun InterProfileScreen(
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+    onNavigateToInterProfileEdit: () -> Unit,
+    viewModel: InterProfileViewModel = hiltViewModel()
 ) {
     Scaffold(
         topBar = {
             ConnectDogTopAppBar(
-                titleRes = null,
+                titleRes = R.string.inter_profile,
                 navigationType = TopAppBarNavigationType.BACK,
                 navigationIconContentDescription = "Navigation icon",
                 onNavigationClick = onBackClick
             )
         }
     ) {
-        Content()
+        Content(
+            onNavigateToInterProfileEdit = onNavigateToInterProfileEdit,
+            viewModel = viewModel
+        )
     }
 }
 
 @Composable
-private fun Content() {
-    Column {
-        Spacer(modifier = Modifier.height(80.dp))
-        IntermediatorProfile()
-    }
-}
+private fun Content(
+    onNavigateToInterProfileEdit: () -> Unit,
+    viewModel: InterProfileViewModel
+) {
+    val reviewUiState by viewModel.interProfileReviewUiState.collectAsStateWithLifecycle()
+    val findingUiState by viewModel.interProfileFindingUiState.collectAsStateWithLifecycle()
 
-@Composable
-fun IntermediatorProfile() {
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(80.dp))
         NetworkImage(imageUrl = "", modifier = Modifier.size(80.dp))
         Spacer(modifier = Modifier.height(12.dp))
-        ConnectDogOutlinedButton(
-            width = 51,
-            height = 14,
-            text = "봉사 3회 진행",
-            padding = 4,
-            onClick = { /*TODO*/ }
-        )
         Spacer(modifier = Modifier.height(12.dp))
         Text("중개단체 이름", fontSize = 18.sp, color = Gray1, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(8.dp))
@@ -96,14 +106,32 @@ fun IntermediatorProfile() {
             modifier = Modifier.widthIn(min = 0.dp, max = 240.dp),
             lineHeight = 15.sp
         )
+        Spacer(modifier = Modifier.height(12.dp))
+        ConnectDogBottomButton(
+            onClick = onNavigateToInterProfileEdit,
+            content = "프로필 수정",
+            textColor = Gray1,
+            enabledColor = Gray7,
+            modifier = Modifier
+                .height(40.dp)
+                .padding(horizontal = 20.dp),
+            fontSize = 12,
+            paddingValues = PaddingValues(vertical = 11.dp)
+        )
         Spacer(modifier = Modifier.height(32.dp))
-        TabLayout()
+        TabLayout(
+            finding = findingUiState,
+            review = reviewUiState
+        )
     }
 }
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun TabLayout() {
+private fun TabLayout(
+    finding: InterProfileFindingUiState,
+    review: InterProfileReviewUiState
+) {
     Surface {
         Column {
             val pagerState = rememberPagerState()
@@ -138,9 +166,9 @@ fun TabLayout() {
                 state = pagerState
             ) {
                 when (it) {
-                    0 -> Information()
-                    1 -> Review()
-                    2 -> News()
+                    0 -> IntermediatorInformation()
+                    1 -> Finding(finding)
+                    2 -> CompleteAndReview(review)
                 }
             }
         }
@@ -148,23 +176,13 @@ fun TabLayout() {
 }
 
 @Composable
-private fun Information() {
+private fun IntermediatorInformation() {
+    val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
+            .padding(all = 24.dp)
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-    ) {
-        IntermediatorInformation()
-        Spacer(modifier = Modifier.weight(1f))
-        ConnectDogNormalButton(content = "수정하기", modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp))
-        Spacer(modifier = Modifier.height(32.dp))
-    }
-}
-
-@Composable
-fun IntermediatorInformation() {
-    Column(
-        modifier = Modifier.padding(all = 24.dp),
+            .verticalScroll(scrollState),
         verticalArrangement = Arrangement.Top
     ) {
         Text(
@@ -182,21 +200,59 @@ fun IntermediatorInformation() {
 }
 
 @Composable
-fun Review() {
+private fun Finding(
+    data: InterProfileFindingUiState
+) {
     val modifier = Modifier.padding(horizontal = 0.dp)
     Column(
         verticalArrangement = Arrangement.Top
     ) {
+        when (data) {
+            is InterProfileFindingUiState.Loading -> Loading()
+            is InterProfileFindingUiState.Empty -> {}
+            is InterProfileFindingUiState.InterProfileFinding -> {
+                LazyColumn {
+                    items(data.data) {
+                        AnnouncementContent(
+                            postId = it.postId,
+                            imageUrl = it.imageUrl,
+                            dogName = it.dogName,
+                            location = it.location,
+                            isKennel = it.isKennel,
+                            dogSize = it.dogSize,
+                            date = it.date,
+                            pickUpTime = it.pickUpTime,
+                            onClick = {}
+                        )
+                    }
+                }
+            }
+        }
+
 //        ReviewLoading(modifier = modifier)
     }
 }
 
 @Composable
-fun News() {
+fun CompleteAndReview(
+    data: InterProfileReviewUiState
+) {
     Column(
         modifier = Modifier.padding(all = 24.dp),
         verticalArrangement = Arrangement.Top
     ) {
+        when (data) {
+            is InterProfileReviewUiState.Loading -> Loading()
+            is InterProfileReviewUiState.Empty -> {
+            }
+            is InterProfileReviewUiState.InterProfileReview -> {
+                LazyColumn(modifier = Modifier.padding(horizontal = 0.dp)) {
+                    items(data.review.take(30)) {
+                        ReviewItemContent(review = it)
+                    }
+                }
+            }
+        }
     }
 }
 
