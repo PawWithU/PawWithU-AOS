98080bec29741af9e33f0d4616e4090324ddb50b
Seunggu Kang <alex9366@naver.com>
2024-05-15 16:47:26 +0900

    feat: 모집자 프로필 화면에서 사용하기 위한 repository 추가

---
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/InterProfileRepository.kt
@@ -0,0 +1,9 @@
+package com.kusitms.connectdog.core.data.repository
+
+import com.kusitms.connectdog.core.model.Announcement
+import com.kusitms.connectdog.core.model.Review
+
+interface InterProfileRepository {
+    suspend fun getInterReview(page: Int?, size: Int?): List<Review>
+    suspend fun getInterFinding(page: Int, size: Int?): List<Announcement>
+}
---
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/InterProfileRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.kusitms.connectdog.core.data.repository
+
+import com.kusitms.connectdog.core.data.api.InterApiService
+import com.kusitms.connectdog.core.data.mapper.intermediator.toData
+import com.kusitms.connectdog.core.data.mapper.toData
+import com.kusitms.connectdog.core.model.Announcement
+import com.kusitms.connectdog.core.model.Review
+import javax.inject.Inject
+
+internal class InterProfileRepositoryImpl @Inject constructor(
+    private val apiService: InterApiService
+) : InterProfileRepository {
+    override suspend fun getInterReview(page: Int?, size: Int?): List<Review> {
+        return apiService.getIntermediatorReview(page, size).map { it.toData() }
+    }
+
+    override suspend fun getInterFinding(page: Int, size: Int?): List<Announcement> {
+        return apiService.getFindingApplication(page, size).map { it.toData() }
+    }
+}
