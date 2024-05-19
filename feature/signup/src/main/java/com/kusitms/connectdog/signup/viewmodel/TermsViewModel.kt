19d07dfff9f0103bc3b22ec201dfbe2e1756a462
Seunggu Kang <alex9366@naver.com>
2024-05-19 15:31:07 +0900

    feat: 선택성 약관 동의 항목 추가

--- feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/TermsViewModel.kt
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/TermsViewModel.kt
@@ -20,28 +20,40 @@ class TermsViewModel @Inject constructor() : ViewModel() {
     private val _advertisementChecked = MutableLiveData(false)
     val advertisementChecked: LiveData<Boolean> = _advertisementChecked
 
+    private val _isAvailableNext = MutableLiveData(false)
+    val isAvailableNext: LiveData<Boolean>
+        get() = _isAvailableNext
+
     fun updateAllChecked() {
         _allChecked.value = !_allChecked.value!!
         _termsChecked.value = !_allChecked.value!!
         _privacyChecked.value = !_allChecked.value!!
+        _advertisementChecked.value = !_allChecked.value!!
     }
 
     fun updateTermsChecked() {
         _termsChecked.value = !_termsChecked.value!!
         updateAllCheckedState()
+        isAvailableNext()
     }
 
     fun updatePrivacyChecked() {
         _privacyChecked.value = !_privacyChecked.value!!
         updateAllCheckedState()
+        isAvailableNext()
     }
 
     fun updateAdvertisementChecked() {
         _advertisementChecked.value = !_advertisementChecked.value!!
+        updateAllCheckedState()
     }
 
     private fun updateAllCheckedState() {
-        _allChecked.value = _termsChecked.value == true && _privacyChecked.value == true
+        _allChecked.value = _termsChecked.value == true && _privacyChecked.value == true && _advertisementChecked.value == true
+    }
+
+    private fun isAvailableNext() {
+        _isAvailableNext.value = _termsChecked.value == true && _privacyChecked.value == true
     }
 
     fun resetState() {
